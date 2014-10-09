08c325be796c963df56f1399d3161537a4b67d21
Johannes Fürmann <johannes@weltraumpflege.org>
2014-10-09 03:55:29 +0200

    add http server

--- sphincterd.py
+++ sphincterd.py
@@ -2,15 +2,19 @@
 
 import logging
 from time import sleep
+
 from sphincter.serial_connection import SphincterSerialHandler
+from sphincter.httpserver import SphincterHTTPServerRunner
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(threadName)s/%(funcName)s - %(levelname)s - %(message)s',
+                        format='%(asctime)s - %(levelname)8s - %(threadName)s/%(funcName)s - %(message)s',
                         datefmt="%Y-%m-%d %H:%M")
     logging.info("ohai, this is sphincterd")
     s = SphincterSerialHandler(device='/dev/ttyACM0')
     s.connect()
+    SphincterHTTPServerRunner.start_thread(('localhost', 1337), s)
+    
     
     # sleep until CTRL-C, then quit.
     try:
