dd530ce065069fd31a4f332e0bd7c05c1c9fc986
Johannes Fürmann <johannes@weltraumpflege.org>
2014-10-23 20:52:07 +0200

    custom hooks

--- hooks.py
+++ hooks.py
@@ -1,12 +1,39 @@
 #
 # hooks will be executed when the corresponding event occurs
 # 
+import logging
 
 def open_hook():
-    pass
-
+    logging.info("Starting OpenHook")
+    import requests
+    import json
+    url = "https://api.openlab-augsburg.de/13/edit/door/"
+    data = {
+        "token": "n8TvfFOciPrcJ67Vn3tTlI7",
+        "status": True
+    }
+    headers = {
+        'Content-type': 'application/json', 
+        'Accept': 'text/plain'
+    }
+    requests.post(url, data=json.dumps(data), headers=headers)
+    logging.info("Finishing OpenHook")
+    
 def closed_hook():
-    pass
+    logging.info("Starting ClosedHook")
+    import requests
+    import json
+    url = "https://api.openlab-augsburg.de/13/edit/door/"
+    data = {
+        "token": "n8TvfFOciPrcJ67Vn3tTlI7",
+        "status": False
+    }
+    headers = {
+        'Content-type': 'application/json', 
+        'Accept': 'text/plain'
+    }
+    requests.post(url, data=json.dumps(data), headers=headers)
+    logging.info("Finishing ClosedHook")
 
 def failure_hook():
     pass
